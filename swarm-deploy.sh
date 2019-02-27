2c1c24b69b3cdfd1e8983cc677cfeb652cdade67
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2019-02-27 13:57:06 +0100

    using docker-machine w/o container

--- swarm-deploy.sh
+++ swarm-deploy.sh
@@ -3,39 +3,43 @@ set -e
 
 CSIZE=${1:-1}
 
-yes y | ssh-keygen -q -t rsa -N ''  -f ~/.ssh/id_rsa 2>&1 > /dev/null
+DM_VER=v0.16.1
+DM_BIN=$HOME/docker-machine
+
+base=https://github.com/docker/machine/releases/download/$DM_VER
+curl -L $base/docker-machine-$(uname -s)-$(uname -m) >$DM_BIN
+chmod +x $DM_BIN
+
+if [ ! -f $HOME/.ssh/id_rsa ]; then
+    yes y | ssh-keygen -q -t rsa -N '' -f $HOME/.ssh/id_rsa 2>&1 > /dev/null
+fi
 
 MNAME=dockermachine-${TRAVIS_BUILD_ID}
-DCMD="docker run --rm --name docker-machine 
-    -v $HOME/.docker/machine:/root/.docker/machine 
-    -v $HOME/.ssh:/root/.ssh/ 
-    nuvla/job-docker-machine-client:test"
 CREATE_CMD="create --driver exoscale 
     --exoscale-ssh-user root 
-    --exoscale-ssh-key /root/.ssh/id_rsa 
+    --exoscale-ssh-key $HOME/.ssh/id_rsa
     --exoscale-api-key ${EXOSCALE_API_KEY} 
     --exoscale-api-secret-key ${EXOSCALE_API_SECRET} 
     --exoscale-availability-zone CH-GVA-2 
     --exoscale-instance-profile Large 
     --exoscale-security-group slipstream_managed"
-$DCMD $CREATE_CMD --exoscale-image 'Linux Ubuntu 16.04 LTS 64-bit' $MNAME 
-ip=$($DCMD ip $MNAME)
-$DCMD ssh $MNAME "sudo docker swarm init --force-new-cluster --advertise-addr $ip"
+
+echo "::: Provisioning master: $MNAME"
+$DM_BIN $CREATE_CMD --exoscale-image 'Linux Ubuntu 16.04 LTS 64-bit' $MNAME
+ip=$($DM_BIN ip $MNAME)
+$DM_BIN ssh $MNAME "sudo docker swarm init --force-new-cluster --advertise-addr $ip"
 
 if [ $CSIZE -gt 1 ]; then
-    joinToken=$($DCMD ssh $MNAME "sudo docker swarm join-token worker -q" | tr -d '\r')
+    joinToken=$($DM_BIN ssh $MNAME "sudo docker swarm join-token worker -q" | tr -d '\r')
     for i in `seq 1 $(($CSIZE - 1))`; do
         WNAME=${MNAME}-worker${i}
         echo "::: Provisioning worker: $WNAME"
-        $DCMD $CREATE_CMD --exoscale-image 'Linux Ubuntu 16.04 LTS 64-bit' $WNAME
-        $DCMD ssh $WNAME "sudo docker swarm join --token ${joinToken} ${ip}:2377"
+        $DM_BIN $CREATE_CMD --exoscale-image 'Linux Ubuntu 16.04 LTS 64-bit' $WNAME
+        $DM_BIN ssh $WNAME "sudo docker swarm join --token ${joinToken} ${ip}:2377"
     done
 fi
 
-ls -alR $HOME/.docker/machine/machines/
-sudo chown -R `id -u`: $HOME/.docker/machine/machines/
-
-$DCMD ssh $MNAME "sudo docker node ls" 
+$DM_BIN ssh $MNAME "sudo docker node ls"
 
 DSTACK_CMD="docker -H $ip:2376 --tls 
     --tlscacert $HOME/.docker/machine/machines/$MNAME/ca.pem
@@ -49,4 +53,4 @@ $DSTACK_CMD ls
 $DSTACK_CMD services $STACK_NAME
 $DSTACK_CMD rm $STACK_NAME
 
-$DCMD rm -y `$DCMD ls -q`
+$DM_BIN rm -y `$DM_BIN ls -q`
