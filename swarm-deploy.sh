1c731999f181000004a51d61a3bd1730c0c8983f
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2019-02-28 15:58:54 +0100

    docker-compose uses configs and secrets for traefik; separated cluster deploy / test / terminate actions

--- swarm-deploy.sh
+++ swarm-deploy.sh
@@ -7,13 +7,17 @@ cleanup() {
 
 trap cleanup ERR
 
-CSIZE=${1:-1}
+action_err_msg="Action required: deploy|termiate"
+ACTION=${1:?$action_err_msg}
+SWSIZE=${2:-1}
 
 DM_VER=v0.16.1
 DM_BIN=$HOME/docker-machine
 
-base=https://github.com/docker/machine/releases/download/$DM_VER
-curl -L $base/docker-machine-$(uname -s)-$(uname -m) >$DM_BIN
+if [ ! -f $DM_BIN ]; then
+    base=https://github.com/docker/machine/releases/download/$DM_VER
+    curl -L $base/docker-machine-$(uname -s)-$(uname -m) >$DM_BIN
+fi
 chmod +x $DM_BIN
 
 if [ ! -f $HOME/.ssh/id_rsa ]; then
@@ -30,33 +34,52 @@ CREATE_CMD="create --driver exoscale
     --exoscale-instance-profile Large 
     --exoscale-security-group slipstream_managed"
 
-echo "::: Provisioning master: $MNAME"
-$DM_BIN $CREATE_CMD --exoscale-image 'Linux Ubuntu 16.04 LTS 64-bit' $MNAME
-ip=$($DM_BIN ip $MNAME)
-$DM_BIN ssh $MNAME "sudo docker swarm init --force-new-cluster --advertise-addr $ip"
+deploy() {
+    swsize=${1:-1}
+    echo "::: Provisioning master: $MNAME"
+    $DM_BIN $CREATE_CMD --exoscale-image 'Linux Ubuntu 16.04 LTS 64-bit' $MNAME
+    ip=$($DM_BIN ip $MNAME)
+    $DM_BIN ssh $MNAME "sudo docker swarm init --force-new-cluster --advertise-addr $ip"
+    
+    if [ $swsize -gt 1 ]; then
+        joinToken=$($DM_BIN ssh $MNAME "sudo docker swarm join-token worker -q" | tr -d '\r')
+        for i in `seq 1 $(($swsize - 1))`; do
+            WNAME=${MNAME}-worker${i}
+            echo "::: Provisioning worker: $WNAME"
+            $DM_BIN $CREATE_CMD --exoscale-image 'Linux Ubuntu 16.04 LTS 64-bit' $WNAME
+            $DM_BIN ssh $WNAME "sudo docker swarm join --token ${joinToken} ${ip}:2377"
+        done
+    fi
+    
+    $DM_BIN ssh $MNAME "sudo docker node ls"
 
-if [ $CSIZE -gt 1 ]; then
-    joinToken=$($DM_BIN ssh $MNAME "sudo docker swarm join-token worker -q" | tr -d '\r')
-    for i in `seq 1 $(($CSIZE - 1))`; do
-        WNAME=${MNAME}-worker${i}
-        echo "::: Provisioning worker: $WNAME"
-        $DM_BIN $CREATE_CMD --exoscale-image 'Linux Ubuntu 16.04 LTS 64-bit' $WNAME
-        $DM_BIN ssh $WNAME "sudo docker swarm join --token ${joinToken} ${ip}:2377"
-    done
+    DSTACK_CMD="docker -H $ip:2376 --tls 
+        --tlscacert $HOME/.docker/machine/machines/$MNAME/ca.pem
+        --tlskey $HOME/.docker/machine/machines/$MNAME/key.pem
+        --tlscert $HOME/.docker/machine/machines/$MNAME/cert.pem
+        stack"
+    
+    STACK_NAME=test
+    $DSTACK_CMD deploy --compose-file $STACK_NAME/docker-compose.yml $STACK_NAME
+    $DSTACK_CMD ls
+    $DSTACK_CMD services $STACK_NAME
+    echo $ip > $HOME/nuvla-test-host
+}
+
+terminate() {
+    machines=`$DM_BIN ls -q`
+    if [ -z "$machines" ]; then
+        echo "WARNING: no machines to terminate"
+    else
+        $DM_BIN rm -y $machines
+    fi
+}
+
+if [ "$ACTION" == "deploy" ]; then
+    deploy $SWSIZE
+elif [ "$ACTION" == "terminate" ]; then
+    terminate
+else
+    echo $action_err_msg
+    exit 1
 fi
-
-$DM_BIN ssh $MNAME "sudo docker node ls"
-
-DSTACK_CMD="docker -H $ip:2376 --tls 
-    --tlscacert $HOME/.docker/machine/machines/$MNAME/ca.pem
-    --tlskey $HOME/.docker/machine/machines/$MNAME/key.pem
-    --tlscert $HOME/.docker/machine/machines/$MNAME/cert.pem
-    stack"
-
-STACK_NAME=test
-$DSTACK_CMD deploy --compose-file $STACK_NAME/docker-compose.yml $STACK_NAME
-$DSTACK_CMD ls
-$DSTACK_CMD services $STACK_NAME
-$DSTACK_CMD rm $STACK_NAME
-
-$DM_BIN rm -y `$DM_BIN ls -q`
