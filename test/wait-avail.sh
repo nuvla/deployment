650592c5d67afdbbbafea35b51032dc0499f45bd
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2019-03-04 08:17:50 +0100

    added wait for cloud-entry-point

---
+++ test/wait-avail.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+endpoint=${1:?"Endpoint required: http[s]://host[:port][/resource]"}
+wait_min_max=${2:-2}
+time_max=$(( $(date +%s) + $(($wait_min_max * 60)) ))
+cmd="curl -k --connect-timeout 5 --max-time 10 -sfS $endpoint"
+set +e
+$cmd
+rc=$?
+set -e
+while [ "$rc" -ne 0 -a "$(date +%s)" -lt "$time_max" ]; do
+    sleep 5
+    set +e
+    $cmd
+    rc=$?
+    set -e
+done
+[ "$rc" -ne 0 ] && echo "ERROR: failed conecting to $endpoint"
+exit $rc
