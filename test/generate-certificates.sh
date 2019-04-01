2a8dd5db794c4e6d77cb77f182cd2f6187e3153b
Charles Loomis <cal@sixsq.com>
2019-04-01 18:00:11 +0200

    use secrets for session cert/key

---
+++ test/generate-certificates.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# generate the SSL certificate for server
+
+PRIVKEY=traefik/nuvla.key
+PUBKEY=traefik/nuvla.crt
+
+mkdir -p traefik
+openssl req \
+        -newkey rsa:2048 \
+        -nodes \
+        -x509 \
+        -days 14 \
+        -subj "/C=EU/O=nuvla/CN=nuvla-test" \
+        -keyout ${PRIVKEY} \
+        -out ${PUBKEY}
+
+#
+# generate RSA certificate for session token signing
+#
+
+PRIVKEY=session/session.crt
+PUBKEY=session/session.key
+
+mkdir -p session
+rm -f ${PRIVKEY} ${PUBKEY}
+openssl genrsa -out ${PRIVKEY} 2048
+openssl rsa -pubout -in ${PRIVKEY} -out ${PUBKEY}
+chmod 400 ${PRIVKEY} ${PRIVKEY}
