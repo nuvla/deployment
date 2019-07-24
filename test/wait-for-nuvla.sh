81e26d87fe05d5c5a8267a8a251551a622025fc7
cjdcordeiro <cristovao.cordeiro@sixsq.com>
2019-07-24 10:28:02 +0200

    wait for nuvla

---
+++ test/wait-for-nuvla.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+endpoint=${1:?"Endpoint required: http[s]://host[:port][/resource]"}
+wait_min_max=${2:-5}
+time_max=$(( $(date +%s) + $(($wait_min_max * 60)) ))
+cmd="curl -k -L --connect-timeout 5 --max-time 10 -sfS $endpoint"
+set +e
+$cmd
+rc=$?
+set -e
+while [ "$rc" -ne 0 -a "$(date +%s)" -lt "$time_max" ]; do
+    sleep 5
+    set +e
+    $cmd
+    rc=$?
+    set -e
+done
+[ "$rc" -ne 0 ] && echo "ERROR: failed connecting to $endpoint"
+exit $rc
