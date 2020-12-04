e1add37f2da4c2b02de8a6861bb9aefcd4672714
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2020-12-04 15:07:18 +0100

    example deployment of prod - script and docu

--- prod/README.md
+++ prod/README.md
@@ -6,3 +6,18 @@ facilitate the deployment of a production Nuvla server. See the "Nuvla
 Deployment" section of the [Nuvla
 documentation](https://docs.nuvla.io/dave) for information on how
 to use these.
+
+See `deploy.sh` for an example of steps for deployment of fully  
+functional Nuvla server.
+
+Prerequisites:
+
+* Docker Swarm cluster of 3 nodes.
+    * Minimal settings: 4 CPU / 8 GB RAM / 50 GB Disk
+* Labeled nodes
+    * one node for running Nuvla front-end and back-end layers:
+        * `docker node update --label-add type=frontend <node1>`
+    * one node for running Nuvla async workers layer:
+        * `docker node update --label-add type=worker-job <node2>`
+    * one node for running Nuvla DB layer:
+        * `docker node update --label-add type=worker-db <node3>`
---
+++ prod/deploy.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+set -x
+set -e
+
+#
+# Environment variables.
+#
+
+# Password for Prometheus.
+# User by 'Deploy cluster and services monitoring.'.
+# NB! Example. Set strong password.
+# echo $(htpasswd -nb admin admin) | sed -e s/\\$/\\$\\$/g -e 's/^.*://'
+export ADMIN_HASHED_PASSWORD='$$apr1$$8Pq3X/GB$$dzKJt.YVgR./36IdL8l5s0'
+
+# SMTP configuration for user email notifications.
+# Used by 'Deploy Nuvla core services.'.
+# NB! Example. Set your SMTP configuration.
+export SMTP_HOST=smtp.gmail.com
+export SMTP_PORT=465
+export SMTP_SSL=true
+export SMTP_USER=mailer@example.com
+export SMTP_PASSWORD=password
+
+#
+# Deploy traefik.
+#
+
+docker network create --driver=overlay traefik-public
+
+cd ../swarm/traefik
+./generate-certificates.sh
+docker stack deploy -c docker-compose.yml traefik
+cd -
+
+#
+# Create 'nuvla-backend' attachable network.
+#
+docker network create --driver=overlay --attachable nuvla-backend
+
+#
+# Deploy cluster and services monitoring.
+#
+
+cd ../swarm/monitoring
+docker stack deploy -c docker-compose.yml prometheus
+cd -
+
+#
+# Deploy Nuvla's DB layer.
+#
+
+cd db
+docker stack deploy -c docker-compose.yml db
+cd -
+
+#
+# Deploy event streaming platform and initialise ksqlDB with
+# Nuvla related streams and tables.
+#
+
+cd streams
+docker stack deploy -c docker-compose.single.yml streams
+cd ksqldb
+./deploy.sh
+cd ../../
+
+#
+# Deploy Nuvla core services.
+#
+
+cd core
+./generate-certificates.sh
+docker stack deploy -c docker-compose.yml nuvla
+cd -
