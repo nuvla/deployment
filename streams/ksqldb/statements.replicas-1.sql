b7fdb420b9c9268556bdafa1c01a6f63183dc3de
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2021-02-16 16:31:02 +0100

    fix: remove test streams

--- streams/ksqldb/statements.replicas-1.sql
+++ streams/ksqldb/statements.replicas-1.sql
@@ -329,71 +329,6 @@ SELECT id,
 FROM NB_OFFLINE_NOTIF_S
 WHERE LCASE(method) = 'slack';
 
-
-------------------------------------------------------
--- Notifications trigger on NB load % above threshold
---
-CREATE STREAM NB_LOAD_ABOVE_NOTIF_S
-WITH (PARTITIONS=1, REPLICAS=1, VALUE_FORMAT='JSON')
-AS SELECT
-       subs."method-id" as id,
-       notif.method as method,
-       notif.destination as destination,
-       subs.subs_id as subs_id,
-       subs.name as subs_name,
-       subs.description as subs_description,
-       CONCAT('edge/', SPLIT(AS_VALUE(nb_tlm.id), '/')[2]) as nb_uri,
-       nb_tlm.name as nb_name,
-       nb_tlm.description as nb_description,
-       CONCAT('NB load %', '') as metric,
-       subs.condition as condition,
-       subs."value" as condition_value,
-       ((nb_tlm.resources->cpu->"load" * 100) / nb_tlm.resources->cpu->"capacity") as "VALUE",
-       nb_tlm.timestamp as timestamp
-FROM NB_TELEM_RESOURCES_REKYED_S AS nb_tlm
-JOIN SUBS_NB_LOAD_ABOVE_T AS subs ON subs."resource-id" = nb_tlm.id
-JOIN NOTIFICATION_METHOD_T AS notif ON notif.id = subs."method-id"
-WHERE (ARRAY_CONTAINS(nb_tlm.acl->"owners", subs.owner) OR ARRAY_CONTAINS(nb_tlm.acl->"view-data", subs.owner))
-      AND subs.enabled = true
-      AND (nb_tlm.resources->cpu->"load" * 100) / nb_tlm.resources->cpu->"capacity" > CAST(subs."value" AS INTEGER)
-      EMIT CHANGES;
-
-INSERT INTO NOTIFICATIONS_EMAIL_S
-SELECT id,
-       method,
-       destination,
-       subs_id,
-       subs_name,
-       subs_description,
-       nb_uri as resource_uri,
-       nb_name as resource_name,
-       nb_description as resource_description,
-       metric,
-       condition,
-       CAST(condition_value AS VARCHAR) AS condition_value,
-       CAST("VALUE" AS VARCHAR) AS "VALUE",
-       timestamp
-FROM NB_LOAD_ABOVE_NOTIF_S
-WHERE LCASE(method) = 'email';
-
-INSERT INTO NOTIFICATIONS_SLACK_S
-SELECT id,
-       method,
-       destination,
-       subs_id,
-       subs_name,
-       subs_description,
-       nb_uri as resource_uri,
-       nb_name as resource_name,
-       nb_description as resource_description,
-       metric,
-       condition,
-       CAST(condition_value AS VARCHAR) AS condition_value,
-       CAST("VALUE" as VARCHAR) AS "VALUE",
-       timestamp
-FROM NB_LOAD_ABOVE_NOTIF_S
-WHERE LCASE(method) = 'slack';
-
 ----------------------------------------------------------------
 -- Notifications trigger on NB CPU load % against all thresholds
 CREATE STREAM NB_LOAD_NOTIF_S
@@ -555,7 +490,7 @@ AS SELECT
        subs."value" as condition_value,
        ((nb_tlm.resources->disks[1]->"used" * 100) / nb_tlm.resources->disks[1]->"capacity") as "value",
        nb_tlm.timestamp as timestamp
-FROM NB_TELEM_RESOURCES_REKYED_S_1 AS nb_tlm
+FROM NB_TELEM_RESOURCES_REKYED_S AS nb_tlm
 JOIN SUBS_NB_DISK_T AS subs ON subs."resource-id" = nb_tlm.id
 JOIN NOTIFICATION_METHOD_T AS notif ON notif.id = subs."method-id"
 WHERE (ARRAY_CONTAINS(nb_tlm.acl->"owners", subs.owner) OR ARRAY_CONTAINS(nb_tlm.acl->"view-data", subs.owner))
