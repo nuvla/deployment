2d60ab977b931b2638877f194b3d49931756d26c
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2021-03-07 20:35:44 +0100

    move ksql statements back to single file to simplify deployment

--- streams/ksqldb/statements.sql
+++ streams/ksqldb/statements.sql
@@ -289,3 +289,335 @@ FROM NB_TELEM_RESOURCES_S as tlm
 JOIN NUVLABOX_T as nb ON tlm.parent = nb.id
 PARTITION BY parent
 EMIT CHANGES;
+
+----------------------------------
+--
+-- Subscription to NB state change
+CREATE TABLE SUBS_NB_STATE_T AS
+SELECT
+    "resource-id",
+    LATEST_BY_OFFSET(AS_VALUE(id)) AS subs_id,
+    LATEST_BY_OFFSET(acl->owners[1]) AS owner
+FROM SUBSCRIPTION_S
+WHERE
+    "resource-kind" = 'nuvlabox'
+    AND criteria->kind = 'boolean'
+    AND criteria->metric = 'state'
+GROUP BY "resource-id"
+EMIT CHANGES;
+
+INSERT INTO NOTIFICATIONS_S
+SELECT
+    subs.subs_id as id,
+    AS_VALUE(subs.subs_id) as subs_id,
+    subs_t.name as subs_name,
+    subs_t."method-ids" as method_ids,
+    subs_t.description as subs_description,
+    CONCAT('edge/', SPLIT(AS_VALUE(nb_tlm.id), '/')[2]) as resource_uri,
+    nb_tlm.name as resource_name,
+    nb_tlm.description as resource_description,
+    'NB online' as metric,
+    CAST((nb_tlm.online = true AND nb_tlm.online_prev = false) as VARCHAR) as condition,
+    '' as condition_value,
+    '' as "VALUE",
+    nb_tlm.timestamp as timestamp,
+    (nb_tlm.online = true AND nb_tlm.online_prev = false) as recovery
+FROM NB_TELEM_RESOURCES_REKYED_S AS nb_tlm
+JOIN SUBS_NB_STATE_T AS subs ON subs."resource-id" = nb_tlm.id
+JOIN SUBSCRIPTION_T AS subs_t ON subs_t.id = subs.subs_id
+WHERE
+    subs_t.enabled = true
+    AND subs_t.category = 'notification'
+    AND subs_t."resource-kind" = 'nuvlabox'
+    AND (ARRAY_CONTAINS(nb_tlm.acl->"owners", subs.owner) OR ARRAY_CONTAINS(nb_tlm.acl->"view-data", subs.owner))
+    AND ((nb_tlm.online = true AND nb_tlm.online_prev = false)
+          OR (nb_tlm.online = false AND nb_tlm.online_prev = true))
+EMIT CHANGES;
+
+--------------------------------------
+--
+-- Subscription to NB load > threshold
+CREATE TABLE SUBS_NB_LOAD_ABOVE_T AS
+SELECT
+    "resource-id",
+    LATEST_BY_OFFSET(AS_VALUE(id)) AS subs_id,
+    LATEST_BY_OFFSET(acl->owners[1]) AS owner
+FROM SUBSCRIPTION_S
+WHERE
+    "resource-kind" = 'nuvlabox'
+    AND criteria->kind = 'numeric'
+    AND criteria->metric = 'load'
+    AND criteria->condition = '>'
+GROUP BY "resource-id"
+EMIT CHANGES;
+
+INSERT INTO NOTIFICATIONS_S
+SELECT
+    subs.subs_id as id,
+    AS_VALUE(subs.subs_id) as subs_id,
+    subs_t.name as subs_name,
+    subs_t."method-ids" as method_ids,
+    subs_t.description as subs_description,
+    CONCAT('edge/', SPLIT(AS_VALUE(nb_tlm.id), '/')[2]) as resource_uri,
+    nb_tlm.name as resource_name,
+    nb_tlm.description as resource_description,
+    'NB load %' as metric,
+    subs_t.criteria->condition as condition,
+    CAST(subs_t.criteria->"value" as VARCHAR) as condition_value,
+    CAST(nb_tlm.resources_cpu_load_pers as VARCHAR) as "VALUE",
+    nb_tlm.timestamp as timestamp,
+    (nb_tlm.resources_prev->cpu->"load" > nb_tlm.resources->cpu->"load") as recovery
+FROM NB_TELEM_RESOURCES_REKYED_S AS nb_tlm
+JOIN SUBS_NB_LOAD_ABOVE_T AS subs ON subs."resource-id" = nb_tlm.id
+JOIN SUBSCRIPTION_T AS subs_t ON subs_t.id = subs.subs_id
+WHERE
+    subs_t.enabled = true
+    AND subs_t.category = 'notification'
+    AND subs_t."resource-kind" = 'nuvlabox'
+    AND (ARRAY_CONTAINS(nb_tlm.acl->"owners", subs.owner) OR ARRAY_CONTAINS(nb_tlm.acl->"view-data", subs.owner))
+    AND ((nb_tlm.resources_prev_cpu_load_pers <= CAST(subs_t.criteria->"value" AS INTEGER)
+           AND nb_tlm.resources_cpu_load_pers > CAST(subs_t.criteria->"value" AS INTEGER))
+        OR
+        (nb_tlm.resources_cpu_load_pers < CAST(subs_t.criteria->"value" AS INTEGER)
+           AND nb_tlm.resources_prev_cpu_load_pers >= CAST(subs_t.criteria->"value" AS INTEGER)))
+EMIT CHANGES;
+
+--------------------------------------
+--
+-- Subscription to NB load < threshold
+CREATE TABLE SUBS_NB_LOAD_BELOW_T AS
+SELECT
+    "resource-id",
+    LATEST_BY_OFFSET(AS_VALUE(id)) AS subs_id,
+    LATEST_BY_OFFSET(acl->owners[1]) AS owner
+FROM SUBSCRIPTION_S
+WHERE
+    "resource-kind" = 'nuvlabox'
+    AND criteria->kind = 'numeric'
+    AND criteria->metric = 'load'
+    AND criteria->condition = '<'
+GROUP BY "resource-id"
+EMIT CHANGES;
+
+INSERT INTO NOTIFICATIONS_S
+SELECT
+    subs.subs_id as id,
+    AS_VALUE(subs.subs_id) as subs_id,
+    subs_t.name as subs_name,
+    subs_t."method-ids" as method_ids,
+    subs_t.description as subs_description,
+    CONCAT('edge/', SPLIT(AS_VALUE(nb_tlm.id), '/')[2]) as resource_uri,
+    nb_tlm.name as resource_name,
+    nb_tlm.description as resource_description,
+    'NB load %' as metric,
+    subs_t.criteria->condition as condition,
+    CAST(subs_t.criteria->"value" as VARCHAR) as condition_value,
+    CAST(nb_tlm.resources_cpu_load_pers as VARCHAR) as "VALUE",
+    nb_tlm.timestamp as timestamp,
+    (nb_tlm.resources_prev->cpu->"load" < nb_tlm.resources->cpu->"load") as recovery
+FROM NB_TELEM_RESOURCES_REKYED_S AS nb_tlm
+JOIN SUBS_NB_LOAD_BELOW_T AS subs ON subs."resource-id" = nb_tlm.id
+JOIN SUBSCRIPTION_T AS subs_t ON subs_t.id = subs.subs_id
+WHERE
+    subs_t.enabled = true
+    AND subs_t.category = 'notification'
+    AND subs_t."resource-kind" = 'nuvlabox'
+    AND (ARRAY_CONTAINS(nb_tlm.acl->"owners", subs.owner) OR ARRAY_CONTAINS(nb_tlm.acl->"view-data", subs.owner))
+    AND ((nb_tlm.resources_prev_cpu_load_pers >= CAST(subs_t.criteria->"value" AS INTEGER)
+           AND nb_tlm.resources_cpu_load_pers < CAST(subs_t.criteria->"value" AS INTEGER))
+        OR
+        (nb_tlm.resources_cpu_load_pers > CAST(subs_t.criteria->"value" AS INTEGER)
+           AND nb_tlm.resources_prev_cpu_load_pers <= CAST(subs_t.criteria->"value" AS INTEGER)))
+EMIT CHANGES;
+
+-------------------------------------
+--
+-- Subscription to NB ram > threshold
+CREATE TABLE SUBS_NB_RAM_ABOVE_T AS
+SELECT
+    "resource-id",
+    LATEST_BY_OFFSET(AS_VALUE(id)) AS subs_id,
+    LATEST_BY_OFFSET(acl->owners[1]) AS owner
+FROM SUBSCRIPTION_S
+WHERE
+    "resource-kind" = 'nuvlabox'
+    AND criteria->kind = 'numeric'
+    AND criteria->metric = 'ram'
+    AND criteria->condition = '>'
+GROUP BY "resource-id"
+EMIT CHANGES;
+
+INSERT INTO NOTIFICATIONS_S
+SELECT
+    subs.subs_id as id,
+    AS_VALUE(subs.subs_id) as subs_id,
+    subs_t.name as subs_name,
+    subs_t."method-ids" as method_ids,
+    subs_t.description as subs_description,
+    CONCAT('edge/', SPLIT(AS_VALUE(nb_tlm.id), '/')[2]) as resource_uri,
+    nb_tlm.name as resource_name,
+    nb_tlm.description as resource_description,
+    'NB ram %' as metric,
+    subs_t.criteria->condition as condition,
+    CAST(subs_t.criteria->"value" as VARCHAR) as condition_value,
+    CAST(nb_tlm.resources_ram_used_pers as VARCHAR) as "VALUE",
+    nb_tlm.timestamp as timestamp,
+    (nb_tlm.resources_prev->ram->"used" > nb_tlm.resources->ram->"used") as recovery
+FROM NB_TELEM_RESOURCES_REKYED_S AS nb_tlm
+JOIN SUBS_NB_RAM_ABOVE_T AS subs ON subs."resource-id" = nb_tlm.id
+JOIN SUBSCRIPTION_T AS subs_t ON subs_t.id = subs.subs_id
+WHERE
+    subs_t.enabled = true
+    AND subs_t.category = 'notification'
+    AND subs_t."resource-kind" = 'nuvlabox'
+    AND (ARRAY_CONTAINS(nb_tlm.acl->"owners", subs.owner) OR ARRAY_CONTAINS(nb_tlm.acl->"view-data", subs.owner))
+    AND ((nb_tlm.resources_prev_ram_used_pers <= CAST(subs_t.criteria->"value" AS INTEGER)
+           AND nb_tlm.resources_ram_used_pers > CAST(subs_t.criteria->"value" AS INTEGER))
+        OR
+        (nb_tlm.resources_ram_used_pers < CAST(subs_t.criteria->"value" AS INTEGER)
+           AND nb_tlm.resources_prev_ram_used_pers >= CAST(subs_t.criteria->"value" AS INTEGER)))
+EMIT CHANGES;
+
+-------------------------------------
+--
+-- Subscription to NB ram < threshold
+CREATE TABLE SUBS_NB_RAM_BELOW_T AS
+SELECT
+    "resource-id",
+    LATEST_BY_OFFSET(AS_VALUE(id)) AS subs_id,
+    LATEST_BY_OFFSET(acl->owners[1]) AS owner
+FROM SUBSCRIPTION_S
+WHERE
+    "resource-kind" = 'nuvlabox'
+    AND criteria->kind = 'numeric'
+    AND criteria->metric = 'ram'
+    AND criteria->condition = '<'
+GROUP BY "resource-id"
+EMIT CHANGES;
+
+INSERT INTO NOTIFICATIONS_S
+SELECT
+    subs.subs_id as id,
+    AS_VALUE(subs.subs_id) as subs_id,
+    subs_t.name as subs_name,
+    subs_t."method-ids" as method_ids,
+    subs_t.description as subs_description,
+    CONCAT('edge/', SPLIT(AS_VALUE(nb_tlm.id), '/')[2]) as resource_uri,
+    nb_tlm.name as resource_name,
+    nb_tlm.description as resource_description,
+    'NB ram %' as metric,
+    subs_t.criteria->condition as condition,
+    CAST(subs_t.criteria->"value" as VARCHAR) as condition_value,
+    CAST(nb_tlm.resources_ram_used_pers as VARCHAR) as "VALUE",
+    nb_tlm.timestamp as timestamp,
+    (nb_tlm.resources_prev->ram->"used" < nb_tlm.resources->ram->"used") as recovery
+FROM NB_TELEM_RESOURCES_REKYED_S AS nb_tlm
+JOIN SUBS_NB_RAM_BELOW_T AS subs ON subs."resource-id" = nb_tlm.id
+JOIN SUBSCRIPTION_T AS subs_t ON subs_t.id = subs.subs_id
+WHERE
+    subs_t.enabled = true
+    AND subs_t.category = 'notification'
+    AND subs_t."resource-kind" = 'nuvlabox'
+    AND (ARRAY_CONTAINS(nb_tlm.acl->"owners", subs.owner) OR ARRAY_CONTAINS(nb_tlm.acl->"view-data", subs.owner))
+    AND ((nb_tlm.resources_prev_ram_used_pers >= CAST(subs_t.criteria->"value" AS INTEGER)
+           AND nb_tlm.resources_ram_used_pers < CAST(subs_t.criteria->"value" AS INTEGER))
+        OR
+        (nb_tlm.resources_ram_used_pers > CAST(subs_t.criteria->"value" AS INTEGER)
+           AND nb_tlm.resources_prev_ram_used_pers <= CAST(subs_t.criteria->"value" AS INTEGER)))
+EMIT CHANGES;
+
+-------------------------------------
+--
+-- Subscription to NB ram > threshold
+CREATE TABLE SUBS_NB_DISK_ABOVE_T AS
+SELECT
+    "resource-id",
+    LATEST_BY_OFFSET(AS_VALUE(id)) AS subs_id,
+    LATEST_BY_OFFSET(acl->owners[1]) AS owner
+FROM SUBSCRIPTION_S
+WHERE
+    "resource-kind" = 'nuvlabox'
+    AND criteria->kind = 'numeric'
+    AND criteria->metric = 'disk'
+    AND criteria->condition = '>'
+GROUP BY "resource-id"
+EMIT CHANGES;
+
+INSERT INTO NOTIFICATIONS_S
+SELECT
+    subs.subs_id as id,
+    AS_VALUE(subs.subs_id) as subs_id,
+    subs_t.name as subs_name,
+    subs_t."method-ids" as method_ids,
+    subs_t.description as subs_description,
+    CONCAT('edge/', SPLIT(AS_VALUE(nb_tlm.id), '/')[2]) as resource_uri,
+    nb_tlm.name as resource_name,
+    nb_tlm.description as resource_description,
+    'NB disk %' as metric,
+    subs_t.criteria->condition as condition,
+    CAST(subs_t.criteria->"value" as VARCHAR) as condition_value,
+    CAST(nb_tlm.resources_disk1_used_pers as VARCHAR) as "VALUE",
+    nb_tlm.timestamp as timestamp,
+    (nb_tlm.resources_prev->disks[1]->"used" > nb_tlm.resources->disks[1]->"used") as recovery
+FROM NB_TELEM_RESOURCES_REKYED_S AS nb_tlm
+JOIN SUBS_NB_DISK_ABOVE_T AS subs ON subs."resource-id" = nb_tlm.id
+JOIN SUBSCRIPTION_T AS subs_t ON subs_t.id = subs.subs_id
+WHERE
+    subs_t.enabled = true
+    AND subs_t.category = 'notification'
+    AND subs_t."resource-kind" = 'nuvlabox'
+    AND (ARRAY_CONTAINS(nb_tlm.acl->"owners", subs.owner) OR ARRAY_CONTAINS(nb_tlm.acl->"view-data", subs.owner))
+    AND ((nb_tlm.resources_prev_disk1_used_pers <= CAST(subs_t.criteria->"value" AS INTEGER)
+           AND nb_tlm.resources_disk1_used_pers > CAST(subs_t.criteria->"value" AS INTEGER))
+        OR
+        (nb_tlm.resources_disk1_used_pers < CAST(subs_t.criteria->"value" AS INTEGER)
+           AND nb_tlm.resources_prev_disk1_used_pers >= CAST(subs_t.criteria->"value" AS INTEGER)))
+EMIT CHANGES;
+
+--------------------------------------
+--
+-- Subscription to NB disk < threshold
+CREATE TABLE SUBS_NB_DISK_BELOW_T AS
+SELECT
+    "resource-id",
+    LATEST_BY_OFFSET(AS_VALUE(id)) AS subs_id,
+    LATEST_BY_OFFSET(acl->owners[1]) AS owner
+FROM SUBSCRIPTION_S
+WHERE
+    "resource-kind" = 'nuvlabox'
+    AND criteria->kind = 'numeric'
+    AND criteria->metric = 'disk'
+    AND criteria->condition = '<'
+GROUP BY "resource-id"
+EMIT CHANGES;
+
+INSERT INTO NOTIFICATIONS_S
+SELECT
+    subs.subs_id as id,
+    AS_VALUE(subs.subs_id) as subs_id,
+    subs_t.name as subs_name,
+    subs_t."method-ids" as method_ids,
+    subs_t.description as subs_description,
+    CONCAT('edge/', SPLIT(AS_VALUE(nb_tlm.id), '/')[2]) as resource_uri,
+    nb_tlm.name as resource_name,
+    nb_tlm.description as resource_description,
+    'NB disk %' as metric,
+    subs_t.criteria->condition as condition,
+    CAST(subs_t.criteria->"value" as VARCHAR) as condition_value,
+    CAST(nb_tlm.resources_disk1_used_pers as VARCHAR) as "VALUE",
+    nb_tlm.timestamp as timestamp,
+    (nb_tlm.resources_prev->disks[1]->"used" < nb_tlm.resources->disks[1]->"used") as recovery
+FROM NB_TELEM_RESOURCES_REKYED_S AS nb_tlm
+JOIN SUBS_NB_DISK_BELOW_T AS subs ON subs."resource-id" = nb_tlm.id
+JOIN SUBSCRIPTION_T AS subs_t ON subs_t.id = subs.subs_id
+WHERE
+    subs_t.enabled = true
+    AND subs_t.category = 'notification'
+    AND subs_t."resource-kind" = 'nuvlabox'
+    AND (ARRAY_CONTAINS(nb_tlm.acl->"owners", subs.owner) OR ARRAY_CONTAINS(nb_tlm.acl->"view-data", subs.owner))
+    AND ((nb_tlm.resources_prev_disk1_used_pers >= CAST(subs_t.criteria->"value" AS INTEGER)
+           AND nb_tlm.resources_disk1_used_pers < CAST(subs_t.criteria->"value" AS INTEGER))
+        OR
+        (nb_tlm.resources_disk1_used_pers > CAST(subs_t.criteria->"value" AS INTEGER)
+           AND nb_tlm.resources_prev_disk1_used_pers <= CAST(subs_t.criteria->"value" AS INTEGER)))
+EMIT CHANGES;
