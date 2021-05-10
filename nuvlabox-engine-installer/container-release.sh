259aff35e1028132dd1148c6287f84f252e13f10
Cristovao Cordeiro <cris@sixsq.com>
2021-05-10 15:58:03 +0200

    Update NuvlaBox Engine version to 2.0.0

--- nuvlabox-engine-installer/container-release.sh
+++ nuvlabox-engine-installer/container-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 
-DOCKER_IMAGE=installer
+DOCKER_IMAGE=2.0.0
 
 # default env vars in GH actions
 GIT_BRANCH=$(echo ${GITHUB_REF} | awk -F'/' '{print $(NF)}' | sed -e 's/[^a-z0-9\._-]/-/g')
